26fe007381f8955d3657490ddf163cc3ce3eb01f
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 20:49:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var array3 = []
+  var array = []
   for(let i = 0; i < array1.length; i++){
     var beatleList = `${i} plays ${instruents[i]}`
-    array3.push(beatleList)
+    array.push(beatleList)
   }
 }
