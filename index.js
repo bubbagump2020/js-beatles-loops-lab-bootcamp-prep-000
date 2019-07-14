e2d2da001be1c1c049ede043ea39c85b74a65808
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 20:48:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
-function theBeatlesPlay(array1, array2){
+function theBeatlesPlay(musicians, instruments){
   var array3 = []
   for(let i = 0; i < array1.length; i++){
-    var beatleList = `${i} plays ${array2[i]}`
+    var beatleList = `${i} plays ${instruents[i]}`
     array3.push(beatleList)
   }
 }
