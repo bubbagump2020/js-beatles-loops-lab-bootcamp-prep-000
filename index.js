4b6fb51ab45974b60e54add1994b4a403b699924
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 20:45:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(array1, array2){
   var array3 = []
   for(let i = 0; i < array1.length; i++){
-    var beatleList = `${i} plays ${array2[0]}`
+    var beatleList = `${i} plays ${array2[i]}`
     array3.push(beatleList)
   }
 }
